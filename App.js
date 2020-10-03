7992f9d0245c940099510eba93ec1e105f03833e
TheLegendaryJedi <goncalo.atanasio@gmail.com>
2020-10-03 17:37:57 +0100

    [NEW] - Welcome screen

--- App.js
+++ App.js
@@ -1,37 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import data from './assets/data/nasdaq.json';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   /* jshint ignore:start */
-
-  const [nasdaqData, setNasdaqData] = useState([])
-
-
-  function fetchNasdaqData() {
-    setNasdaqData(data);
-  }
-
-  useEffect(()=> {
-    fetchNasdaqData();
-  })
   return (
-      <FlatList 
-      data={nasdaqData}
-      contentContainerStyle={styles.container}
-      keyExtractor={item => item.Symbol}
-  renderItem={({item}) => <Text style={styles.text}>{item.Symbol} - {item["Company Name"]}</Text>}
-      />
-    );
-/* jshint ignore:end */
+    <WelcomeScreen/>
+  );
+  /* jshint ignore:end */
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
